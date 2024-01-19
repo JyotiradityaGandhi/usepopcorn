fb03f843cc59dd6ff0782879e7a7aa874bb4018d
Jyotiraditya Gandhi <jyotiraditya602@gmail.com>
2024-01-19 14:37:47 +0530

    new file:   src/app-v2.js 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App-v1";
+import App from "./app-v2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
